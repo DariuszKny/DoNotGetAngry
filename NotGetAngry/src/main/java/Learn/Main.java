ae410d02c1cd84b3ebd11b6f1743255911f0cbd9
DariuszKny <dariusz.knysak@gmail.com>
2019-09-25 00:14:29 +0200

    Background solution

--- NotGetAngry/src/main/java/Learn/Main.java
+++ NotGetAngry/src/main/java/Learn/Main.java
@@ -2,11 +2,19 @@ package Learn;
 
 
 import Learn.Board.SetBackGround;
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -34,13 +42,48 @@ public class Main extends Application  {
         String source = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\images\\board1234.png";
          Image image = new Image(new FileInputStream(source));
 
+         String source1 = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\REDPAWN1.png";
+        Image redPawn1 = new Image(new FileInputStream(source1));
+
+        String source2 = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\REDPAWN2.png";
+        Image redPawn2 = new Image(new FileInputStream(source2));
+
+        String source3 = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\REDPAWN3.png";
+        Image redPawn3 = new Image(new FileInputStream(source3));
+
+        String greenPawn1Source = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\GREENPAWN1.png";
+        Image greenPawn1 = new Image(new FileInputStream(greenPawn1Source));
+
+        String greenPawn2Source = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\GREENPAWN2.png";
+        Image greenPawn2 = new Image(new FileInputStream(greenPawn2Source));
+
+        String greenPawn3Source = "C:\\JavaProjects\\Dariusz-Knysak-kodilla-java-NotGetAngry\\NotGetAngry\\src\\main\\resources\\GREENPAWN3.png";
+        Image greenPawn3 = new Image(new FileInputStream(greenPawn3Source));
+
         GridPane grid = new GridPane();
-        grid.setHgap(17);
-        grid.setVgap(16);
-        grid.setPadding(new Insets(40, 22, 22, 45));
+        grid.setHgap(0);
+        grid.setVgap(0);
+        grid.setPadding(new Insets(400, 400, 0, 0));
         grid.setBackground(SetBackGround.setNewBackground(image));
+        grid.setGridLinesVisible(true);
+
+        ImageView img1 = new ImageView(redPawn1);
+        ImageView img2 = new ImageView(redPawn2);
+        ImageView img3 = new ImageView(redPawn3);
+
+        ImageView GreenPawnImg1 = new ImageView(greenPawn1);
+        ImageView GreenPawnImg2 = new ImageView(greenPawn2);
+        ImageView GreenPawnImg3 = new ImageView(greenPawn3);
 
 
+
+        grid.add(img1, 1, 9, 1, 1);
+        grid.add(img2, 2, 9, 1, 1);
+        grid.add(img3, 3, 9, 1, 1);
+
+        grid.add(GreenPawnImg1, 10, 9, 1, 1);
+        grid.add(GreenPawnImg2, 9, 9, 1, 1);
+        grid.add(GreenPawnImg3, 8, 9, 1, 1);
 
 
         Scene scene = new Scene(grid, 600, 600, Color.WHITE);
